0fb303d9dd3a810e388039a39b24adb15a7ff05a
namnguyen215 <namnguyenphuong215@gmail.com>
2022-04-11 09:44:27 +0700

    tuan5

--- SQLInjection/src/main/java/DAO/UserDB.java
+++ SQLInjection/src/main/java/DAO/UserDB.java
@@ -1,6 +1,8 @@
 package DAO;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDB {
     public Connection getConnection() {
@@ -18,7 +20,7 @@ public class UserDB {
 
     public boolean checkUser(String username, String password) {
         String sql = "select * from login where username='" + username + "' and password='" + password+"';";
-        System.out.println(sql);
+
         Connection connection = getConnection();
         Statement ps = null;
         ResultSet result = null;
@@ -43,8 +45,33 @@ public class UserDB {
         return false;
     }
 
+    public void addUser(String username, String password){
+        String sql= "insert into login values ('"+username+"','"+password+"');";
+        System.out.println(sql);
+        Connection connection = getConnection();
+        Statement ps = null;
+        try {
+            ps = connection.createStatement();
+            ps.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Loi syntax");
+        } finally {
+            try {
+                if (connection != null) connection.close();
+                if (ps != null) ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     public static void main(String[] args) {
         UserDB db=new UserDB();
-        System.out.println(db.checkUser("aaa' or 1=1 -- ","123"));
+//        System.out.println(db.checkUser("aaa' or 1=1 -- ","123"));
+//        db.addUser("user2","psw2");
+        System.out.println(db.checkUser("aaa","zxc' or 1=1--"));
+        List<String> ldy=new ArrayList<>();
+        ArrayList<String> ls=new ArrayList<>();
+
     }
 }
